bc9acb9c6df82ec9b0e1d9050b9c560af3a5407f
Sai <chandra27sai@gmail.com>
2024-05-14 21:25:00 +0530

    Modified Service

---
+++ Entity/customer.py
@@ -0,0 +1,53 @@
+class Customer:
+    def __init__(self, customer_id, first_name, last_name, email, phone, address):
+        self.__customer_id = customer_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__email = email
+        self.__phone = phone
+        self.__address = address
+        self.__total_orders = 0
+
+    @property
+    def customer_id(self):
+        return self.__customer_id
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self.__first_name = value
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        self.__last_name = value
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        self.__email = value
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, value):
+        self.__phone = value
+
+    @property
+    def address(self):
+        return self.__address
+
+    @address.setter
+    def address(self, value):
+        self.__address = value
